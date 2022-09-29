5ae89188c48736d3bc18806a932ce472730c4e96
MateyMatev <285041@viauc.dk>
2022-09-29 19:13:28 +0200

    Yes

--- assignment1.sh
+++ assignment1.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 # Command line arguments: $1 = led type :"led0", "led1", "led2", "led3", "all", $2 = mode: "on", "off", "heartbeat", "blink", "default" (
 
 arg1=("led0" "led1" "led2" "led3" "all")
 arg2=("on" "off" "heartbeat" "blink" "default")
 
-led=$1
-mode=$2
+led="$1"
+mode="$2"
 
 displayhelp () {
     echo "Usage: assignment1.sh [led] [mode]"
@@ -16,29 +17,35 @@ displayhelp () {
 }
 
 setblink () {
-    delay=(1000/$1)/2
+	delay=$(((1000/$1)/2))
     echo "timer" > /sys/class/leds/beaglebone:green:$led/trigger
     echo "$delay" > /sys/class/leds/beaglebone:green:$led/delay_on
     echo "$delay" > /sys/class/leds/beaglebone:green:$led/delay_off
-    exit 0
 }
 
 mapled () {
-    case $led in
-        led0)
-            $led = "usr0"
+    case "$led" in
+	led0)
+            led="usr0"
             ;;
         led1)
-            $led = "usr1"
+            led="usr1"
             ;;
         led2)
-            $led = "usr2"
+            led="usr2"
             ;;
         led3)
-            $led = "usr3"
+            led="usr3"
             ;;
     esac
 }
+
+get_led_path() {
+    local led="$1"
+    echo "${led/led/usr}"
+}
+
+var="$(get_let_path "$led")"
 
 mapmode () {
     case $1 in
@@ -52,20 +59,19 @@ mapmode () {
             $mode = "heartbeat"
             ;;
         "default")
-                if [ $2==0 ]; then
+                if [ "$2" -eq 0 ]; then
                     $mode = "heartbeat"
-                elif [ $2==1 ]; then
+                elif [ "$2" -eq 1 ]; then
                     $mode = "mmc0"
-                elif [ $2==2 ]; then
+                elif [ "$2" -eq 2 ]; then
                     $mode = "cpu0"
-                elif [ $2==3 ]; then
+                elif [ "$2" -eq 3 ]; then
                     $mode = "mmc1"
                 fi
             ;;
     esac
 }
 
-cd /
 
 for i in "$@"; do
   case $i in
@@ -76,37 +82,35 @@ for i in "$@"; do
 done
 
 if [ "$#" -ne 2 ]; then
-    echo "Error: Invalid number of arguments"
-    displayhelp
+    echo "Error: Invalid number of arguments" 1>&2
+    displayhelp 1>&2
 fi
 
 if [[ ! " ${arg1[@]} " =~ " ${led} " ]]; then
-    echo "Error: Invalid LED"
-    displayhelp
+    echo "Error: Invalid LED" 1>&2
+    displayhelp 1>&2
 fi
 
 if [[ ! " ${arg2[@]} " =~ " ${mode} " ]]; then
-    echo "Error: Invalid mode"
-    displayhelp
+    echo "Error: Invalid mode" 1>&2
+    displayhelp 1>&2
 fi
 
-if [ mode == "blink" ]; then
+if [ "$mode" = "blink" ]; then
     frequency=$3
     mapled
-    setblink frequency
+    setblink "$frequency"
 fi
 
 
 if [ "$led" = "all" ]; then
     for i in {0..3}
     do
-        mapmode mode  i
+        mapmode "$mode" $i
         echo $mode > /sys/class/leds/beaglebone:green:usr$i/trigger
     done
 else
-    mapmode mode 0
+    mapmode "$mode" 0
     mapled
     echo $mode > /sys/class/leds/beaglebone:green:$led/trigger
 fi
-
-exit 0
