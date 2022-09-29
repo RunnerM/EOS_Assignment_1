bfd64a7630477f4bbec8f57f0aa5fcc555395262
Marton Pentek <pentek.marci@gmail.com>
2022-09-29 12:56:10 +0200

    wip

--- assignment1.sh
+++ assignment1.sh
@@ -6,13 +6,74 @@ arg2=("on" "off" "heartbeat" "blink" "default")
 led=$1
 mode=$2
 
+displayhelp () {
+    echo "Usage: assignment1.sh [led] [mode]"
+    echo "led: led0, led1, led2, led3, all"
+    echo "mode: on, off, heartbeat, blink, default"
+    echo "Example: assignment1.sh led0 on"
+    echo "If blink than provide frequency as third argument"
+    exit 1
+}
+
+setblink () {
+    delay=(1000/$1)/2
+    echo "timer" > /sys/class/leds/beaglebone:green:$led/trigger
+    echo "$delay" > /sys/class/leds/beaglebone:green:$led/delay_on
+    echo "$delay" > /sys/class/leds/beaglebone:green:$led/delay_off
+    exit 0
+}
+
+mapled () {
+    case $led in
+        led0)
+            $led = "usr0"
+            ;;
+        led1)
+            $led = "usr1"
+            ;;
+        led2)
+            $led = "usr2"
+            ;;
+        led3)
+            $led = "usr3"
+            ;;
+    esac
+}
+
+mapmode () {
+    case $1 in
+        "on")
+            $mode = "default-on"
+            ;;
+        "off")
+            $mode = "none"
+            ;;
+        "heartbeat")
+            $mode = "heartbeat"
+            ;;
+        "default")
+                if [ $2==0 ]; then
+                    $mode = "heartbeat"
+                elif [ $2==1 ]; then
+                    $mode = "mmc0"
+                elif [ $2==2 ]; then
+                    $mode = "cpu0"
+                elif [ $2==3 ]; then
+                    $mode = "mmc1"
+                fi
+            ;;
+    esac
+}
+
+cd /
+
 for i in "$@"; do
   case $i in
     -h|--help)
       displayhelp
       ;;
   esac
-done      
+done
 
 if [ "$#" -ne 2 ]; then
     echo "Error: Invalid number of arguments"
@@ -31,62 +92,20 @@ fi
 
 if [ mode == "blink" ]; then
     frequency=$3
-    setblink(frequency)
+    setblink frequency
 fi
 
 
 if [ "$led" = "all" ]; then
     for i in {0..3}
     do
-        mapmode(mode, i)
+        mapmode mode  i
         echo $mode > /sys/class/leds/beaglebone:green:usr$i/trigger
     done
 else
-    mapmode(mode,0)
+    mapmode mode 0
+    mapled
     echo $mode > /sys/class/leds/beaglebone:green:$led/trigger
 fi
 
 exit 0
-
-
-function displayhelp {
-    echo "Usage: assignment1.sh [led] [mode]"
-    echo "led: led0, led1, led2, led3, all"
-    echo "mode: on, off, heartbeat, blink, default"
-    echo "Example: assignment1.sh led0 on"
-    echo "If blink than provide frequency as third argument"
-    exit 1
-}
-
-function setblink (frequency) {
-    delay=(1000/$frequency)/2
-    echo "timer" > /sys/class/leds/beaglebone:green:$led/trigger
-    echo "$delay" > /sys/class/leds/beaglebone:green:$led/delay_on
-    echo "$delay" > /sys/class/leds/beaglebone:green:$led/delay_off
-    exit 0
-}
-
-function mapmode (mode, i) {
-    case $mode in
-        "on")
-            mode = "default-on"
-            ;;
-        "off")
-            mode = "none"
-            ;;
-        "heartbeat")
-            mode = "heartbeat"
-            ;;
-        "default")
-                if i==0
-                    mode = "heartbeat"
-                elif i==1
-                    mode = "mmc0"
-                elif i==2
-                    mode = "cpu0"
-                elif i==3
-                    mode = "mmc1"
-                fi
-            ;;
-    esac
-}
