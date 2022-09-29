658bde978263d56d839eceb1adffb5fe7dd724b4
Marton Pentek <pentek.marci@gmail.com>
2022-09-29 10:18:21 +0200

    wip

--- assignment1.sh
+++ assignment1.sh
@@ -6,19 +6,32 @@ arg2=("on" "off" "heartbeat" "blink" "default")
 led=$1
 mode=$2
 
+for i in "$@"; do
+  case $i in
+    -h|--help)
+      displayhelp
+      ;;
+  esac
+done      
+
 if [ "$#" -ne 2 ]; then
     echo "Error: Invalid number of arguments"
-    exit 1
+    displayhelp
 fi
 
 if [[ ! " ${arg1[@]} " =~ " ${led} " ]]; then
     echo "Error: Invalid LED"
-    exit 1
+    displayhelp
 fi
 
 if [[ ! " ${arg2[@]} " =~ " ${mode} " ]]; then
     echo "Error: Invalid mode"
-    exit 1
+    displayhelp
+fi
+
+if [ mode == "blink" ]; then
+    frequency=$3
+    setblink(frequency)
 fi
 
 if [ "$led" = "all" ]; then
@@ -31,3 +44,20 @@ else
 fi
 
 exit 0
+
+
+function displayhelp {
+    echo "Usage: assignment1.sh [led] [mode]"
+    echo "led: led0, led1, led2, led3, all"
+    echo "mode: on, off, heartbeat, blink, default"
+    echo "Example: assignment1.sh led0 on"
+    echo "If blink than provide frequency as third argument"
+    exit 1
+}
+
+function setblink(frequency) {
+    echo "blink" > /sys/class/leds/beaglebone:green:$led/trigger
+    echo "100" > /sys/class/leds/beaglebone:green:$led/delay_on
+    echo "100" > /sys/class/leds/beaglebone:green:$led/delay_off
+    exit 0
+}
